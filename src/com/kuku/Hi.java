f75c7f7ebc4a08a2dcdab75a0d49d79afd568be6
Anna <marla1@inbox.ru>
2019-01-26 10:31:41 +0200

    Initial commit

---
+++ src/com/kuku/Hi.java
@@ -0,0 +1,61 @@
+package com.kuku;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Hi {
+    public static void main(String[] args) throws IOException {
+        // Task 1: Hello name
+       /* System.out.println("Enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hi " + name);*/
+
+       //Task 2: ReversArgs
+       /* for (int i = args.length - 1; i >= 0; i--) {
+           System.out.print(args[i] + " ");
+       }*/
+
+        //Task 3: Random numbers
+       /* System.out.println("Enter a number:");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        Random rand = new Random();
+
+        for (int i = 0; i <number ; i++) {
+            int n = rand.nextInt(100);
+            System.out.println(n);
+        }
+
+        for (int i = 0; i <number ; i++) {
+            int n = rand.nextInt(100);
+            System.out.print(n + " ");
+        }*/
+
+        //Task 4: Password
+        /*String password = "Password";
+        System.out.println("Enter password:");
+        Scanner scanner = new Scanner(System.in);
+        String userPassword = scanner.nextLine();
+        if (password == userPassword) {
+            System.out.println("You win!!!");
+        }
+        else {
+            System.out.println("You loose");
+        }*/
+
+        //Task 5: Arguments sum
+       /* int sum = 0;
+        for (int i = 0; i < args.length; i++) {
+            sum += Integer.parseInt(args[i]);
+        }
+
+        System.out.println("Summary: " + sum);*/
+
+       //Task 6: Complicated
+
+    }
+}
---
+++ src/com/kuku/Main.java
@@ -0,0 +1,112 @@
+package com.kuku;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        /*System.out.println("Hi");
+        m();
+        System.out.println("Finish");
+        int file = 9;
+        System.out.println(file);
+        int Ann = 10;
+        System.out.println(Ann++);
+        int read = System.in.read();
+        System.out.println((char)read);*/
+        /*Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(s);*/
+       /* char a='D';
+        System.out.println(a/2);
+        System.out.println("\u2122\u0037");*/
+        /*char c1 = '1';
+        char c2 = '\u0031';
+        char c3 = 49;
+        System.out.println(c1+c2+c3);*/
+        /*System.out.println("4/2="+4/2=="4/2=2");*/
+       /* int x = 10;
+        if (x < 20) {
+            System.out.println("x<20");
+        } else {
+            System.out.println("x>20");
+
+        }*/
+       /* int age = 18; // Student
+        String study;
+
+        if (age >= 3 && age <= 6) {
+            study = "kindergarden";
+        } else if (age >= 7 && age <= 17) {
+            study = "school";
+        } else if (age >= 18 && age <= 23) {
+            study = "uneversity";
+        } else if (age >= 24 && age <= 64) {
+            study = "work";
+        } else {
+            study = "relax";
+        }
+
+        System.out.println(study);*/
+        /*int x = 7;
+        switch (x) {
+            case 1:
+            System.out.println("1");
+            break;
+            case 2:
+            System.out.println("2");
+            break;
+            case 3:
+            System.out.println("3");
+            break;
+            case 4:
+            System.out.println("4");
+            break;
+            case 5:
+            System.out.println("5");
+                System.out.println("work day");
+            break;
+            case 6:
+            System.out.println("6");
+            break;
+            case 7:
+                System.out.println("7");
+            System.out.println("off day");
+                break;
+            default:
+                System.out.println("--");
+                }*/
+        /*for (int i = 0; i <=10 ; i++) {
+            System.out.println(i);
+        }*/
+        /*for (int i = 1; i <=100 ; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }*/
+
+        /*for (int i = 1; i <100; i++, System.out.println(i)) {
+
+        }*/
+
+       /* int[] x = {1, 2, 3, 4, 5, 6, 7};
+        for (int i : x) {
+            System.out.println(i);
+
+            }
+
+            for (int i1 = x.length - 1; i1 >= 0; i1--) {
+                System.out.println(x[i1]);
+        }*/
+
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) continue;
+                System.out.println(i+" ");
+        }
+    }
+
+    /*static void m() {
+        System.out.println("Yes");
+    }*/
+}
+
